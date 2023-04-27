d35ae9d8a552397fc42a8d60823d7d7700c6bce7
Shuklaaa <shuklaabhishek0404@gmail.com>
2023-04-27 10:50:48 +0530

    error explanation

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.post('/', async(req, res) => {
         })
     } catch (error){
         console.log(error);
-        res.status(500).send({ error })
+        res.status(500).send({ error })  //500 means the server encountered an unexpected condition that prevented it from fulfilling the request
     }
 })
 

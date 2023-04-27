2926df162903283c545226fb342db5cce67c50e1
Shuklaaa <shuklaabhishek0404@gmail.com>
2023-04-27 10:45:01 +0530

    Code Explanation server side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';  //dotenv reads key value pairs from the .env file and can set them as environment variables
 import cors from 'cors'; //allows us to make cross origin requests
 import { Configuration , OpenAIApi } from 'openai';
 
-dotenv.config();
+dotenv.config();  //loads environment variables from .env file
 
 // console.log(process.env.OPENAI_API_KEY)
 
@@ -17,9 +17,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());  //allows us to send json from froentend to backend
 
-app.get('/', async(req, res) => {
-    res.status(200).send({
-        message: 'Hello from this side',
+app.get('/', async(req, res) => {     //basic response from server
+    res.status(200).send({                 //A Promise is a proxy for a value not necessarily known
+        message: 'Hello from this side',   //async makes promises easier to write
     })
 });
 
@@ -27,12 +27,12 @@ app.post('/', async(req, res) => {
     try{
         const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion({
+        const response = await openai.createCompletion({  //await is usually used to unwrap promises by passing a Promise as the expression
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
+            temperature: 0,   //a parameter of OpenAI ChatGPT, GPT-3 and GPT-4 models that governs the randomness and thus the creativity of the responses
+            max_tokens: 3000,  //The maximum number of tokens to generate in the completion.
+            top_p: 1,  // top_p determines the proportion of the most likely word choices the model should consider when generating text
             frequency_penalty: 0.5,
             presence_penalty: 0,
         });;
